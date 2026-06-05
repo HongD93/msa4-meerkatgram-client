df5637411a0f5ce841bf53fc438b76c49da39454
HongD93 <tigerson93@gmail.com>
2026-06-05 11:58:03 +0900

    260605 [develop] 회원가입 완료

--- src/pages/auth/Registration.vue
+++ src/pages/auth/Registration.vue
@@ -5,6 +5,7 @@ import MyInput from '../../components/input/MyInput.vue';
 import { useFileStore } from '../../store/file/useFileStore.js';
 import { useAuthStore } from '../../store/auth/useAuthStore.js';
 import { useRouter } from 'vue-router';
+import registrationValidator from '../../util/validator/domain/auth/registrationValidator.js';
 
 const router = useRouter();
 const fileStore = useFileStore();
@@ -21,6 +22,22 @@ const registrationData = reactive({
 })
 
 const handleSubmit = async () => {
+  // 유효성 검사
+  const validationList = [
+    registrationValidator.email(registrationData.email),
+    registrationValidator.password(registrationData.password),
+    registrationValidator.passwordChk(registrationData.password, registrationData.passwordChk),
+    registrationValidator.nick(registrationData.nick),
+    registrationValidator.profile(registrationData.profile),
+  ];
+
+  const errorList = validationList.filter(val => val);
+
+  if(errorList.length > 0) {
+    alert(errorList.join('\n'));
+    return;
+  }
+
   try {
     await authStroe.registration(registrationData);
     alert("회원가입에 성공했습니다.");
